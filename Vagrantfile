0ea110592aa76045838785ba5802346ae9b86bb6
agentelinux <krishna@agentelinux.com.br>
2020-01-08 05:52:17 -0300

    update

--- Vagrantfile
+++ Vagrantfile
@@ -18,52 +18,20 @@ TLD = vconfig['vagrant_dns_tld']
 
 servers=[
   {
-    :hostname => "nfsserver." + "#{DOMAIN}",
-    :ip => "#{BRIDGE_NET}" + "150",
-    :ram => 1024,
-    :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh"
-  },
-  {
-    :hostname => "nfsclient." + "#{DOMAIN}",
-    :ip => "#{BRIDGE_NET}" + "151",
-    :ram => 1024,
-    :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh"    
-  },
-  {
-    :hostname => "docker." + "#{DOMAIN}",
-    :ip => "#{BRIDGE_NET}" + "152",
-    :ram => 2048,
-    :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh"    
-  },
-  {
-    :hostname => "jenkins." + "#{DOMAIN}",
-    :ip => "#{BRIDGE_NET}" + "153",
-    :ram => "#{RAM}",
-    :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh",    
-    :guest_j => 80,
-    :host_j => 8080,
-    :sguest_j => 443,
-    :shost_j => 8443 
-  },
-  {
-    :hostname => "gitlab." + "#{DOMAIN}",
-    :ip => "#{BRIDGE_NET}" + "154",
-    :ram => "#{RAM}",
-    :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh", 
-    :guest_g => 80,
-    :host_g => 9080,
-    :sguest_g => 443,
-    :shost_g => 9443 
-  },
-  {
     :hostname => "ansible." + "#{DOMAIN}",
     :ip => "#{BRIDGE_NET}" + "155",
-    :ram => 1024,
+    :ram => 3072,
     :sshd_config => "#{dir}/artefacts/scripts/sshd_config.sh",    
     :install_ansible => "#{dir}/artefacts/scripts/install_ansible.sh", 
     :config_ansible => "#{dir}/artefacts/scripts/config_ansible.sh",
     :source =>  "#{dir}/artefacts/.",
-    :destination => "/home/vagrant/"
+    :destination => "/home/vagrant/",
+    :guest_g => 80,
+    :host_g => 9080,
+    :sguest_g => 443,
+    :shost_g => 9443,
+    :guest_j => 8080,
+    :host_j => 8080
   }
 ]
  
@@ -95,7 +63,7 @@ Vagrant.configure(2) do |config|
 
         if machine[:guest_j].to_s then
           node.vm.network "forwarded_port", guest: machine[:guest_j].to_s , host: machine[:host_j].to_s
-          node.vm.network "forwarded_port", guest: machine[:sguest_j].to_s , host: machine[:shost_j].to_s
+#          node.vm.network "forwarded_port", guest: machine[:sguest_j].to_s , host: machine[:shost_j].to_s
         end
 
         if File.exist? machine[:sshd_config].to_s then
--- artefacts/scripts/config_ansible.sh
+++ artefacts/scripts/config_ansible.sh
@@ -11,40 +11,45 @@ PASSWORD=vagrant
 
 # add addresses to /etc/hosts 
 echo "192.168.99.155 ansible.sample.test" | sudo tee -a /etc/hosts 
-echo "192.168.99.154 gitlab.sample.test" | sudo tee -a /etc/hosts 
-echo "192.168.99.153 jenkins.sample.test" | sudo tee -a /etc/hosts 
-echo "192.168.99.152 docker.sample.test" | sudo tee -a /etc/hosts 
-echo "192.168.99.151 nfsclient.sample.test" | sudo tee -a /etc/hosts
-echo "192.168.99.150 nfsserver.sample.test" | sudo tee -a /etc/hosts  
+#echo "192.168.99.154 gitlab.sample.test" | sudo tee -a /etc/hosts 
+#echo "192.168.99.153 jenkins.sample.test" | sudo tee -a /etc/hosts 
+#echo "192.168.99.152 docker.sample.test" | sudo tee -a /etc/hosts 
+#echo "192.168.99.151 nfsclient.sample.test" | sudo tee -a /etc/hosts
+#echo "192.168.99.150 nfsserver.sample.test" | sudo tee -a /etc/hosts  
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[all]" | sudo tee -a /etc/ansible/hosts
-echo "gitlab.sample.test" | sudo tee -a /etc/ansible/hosts 
-echo "jenkins.sample.test" | sudo tee -a /etc/ansible/hosts 
-echo "docker.sample.test" | sudo tee -a /etc/ansible/hosts 
-echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
-echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts  
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
+#echo "jenkins.sample.test" | sudo tee -a /etc/ansible/hosts 
+#echo "docker.sample.test" | sudo tee -a /etc/ansible/hosts 
+#echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts  
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[test]" | sudo tee -a /etc/ansible/hosts
-echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts
-echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[nfs-server]" | sudo tee -a /etc/ansible/hosts
-echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "nfsserver.sample.test" | sudo tee -a /etc/ansible/hosts
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[nfs-clients]" | sudo tee -a /etc/ansible/hosts
-echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "nfsclient.sample.test" | sudo tee -a /etc/ansible/hosts
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[jenkins]" | sudo tee -a /etc/ansible/hosts
-echo "jenkins.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "jenkins.sample.test" | sudo tee -a /etc/ansible/hosts
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[docker]" | sudo tee -a /etc/ansible/hosts
-echo "docker.sample.test" | sudo tee -a /etc/ansible/hosts
+#echo "docker.sample.test" | sudo tee -a /etc/ansible/hosts
+echo "ansible.sample.test" | sudo tee -a /etc/ansible/hosts 
 
 echo " " | sudo tee -a /etc/ansible/hosts
 echo "[gitlab]" | sudo tee -a /etc/ansible/hosts
@@ -57,14 +62,14 @@ chmod +x /vagrant/artefacts/scripts/ssh_pass.sh
 
 # password less authentication using expect scripting language
 /vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "ansible.sample.test" 
-/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "nfsclient.sample.test" 
-/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "nfsserver.sample.test" 
-/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "docker.sample.test" 
-/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "jenkins.sample.test"
-/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "gitlab.sample.test"
+#/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "nfsclient.sample.test" 
+#/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "nfsserver.sample.test" 
+#/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "docker.sample.test" 
+#/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "jenkins.sample.test"
+#/vagrant/artefacts/scripts/ssh_pass.sh $USER $PASSWORD "gitlab.sample.test"
 
-ansible-playbook /vagrant/artefacts/playbooks/nfs_server.yaml
-ansible-playbook /vagrant/artefacts/playbooks/nfs_clients.yaml
+#ansible-playbook /vagrant/artefacts/playbooks/nfs_server.yaml
+#ansible-playbook /vagrant/artefacts/playbooks/nfs_clients.yaml
 ansible-playbook /vagrant/artefacts/playbooks/install_java.yaml
 ansible-playbook /vagrant/artefacts/playbooks/install_jenkins.yaml
 ansible-playbook /vagrant/artefacts/playbooks/install_docker.yaml
