5108eb8c384a3f211b3ffb9e814d4bc9e0d6e66b
agentelinux <krishna@agentelinux.com.br>
2020-01-12 11:57:38 -0300

    update

--- artefacts/scripts/install_ansible.sh
+++ artefacts/scripts/install_ansible.sh
@@ -8,13 +8,6 @@ apt-add-repository ppa:ansible/ansible -y
 # Install Ansible.
 #apt-get update
 apt-get install ansible -y
-ansible-galaxy install geerlingguy.java
-ansible-galaxy install geerlingguy.jenkins
-ansible-galaxy install geerlingguy.gitlab
-ansible-galaxy install geerlingguy.pip
-ansible-galaxy install geerlingguy.docker
-ansible-galaxy install robertdebock.service
-ansible-galaxy install robertdebock.rundeck
 
 # Install expect, dos2unix & tree
 apt-get install expect -y 
